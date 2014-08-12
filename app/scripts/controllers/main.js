f0eacac6efb32c8d82dc086c240ab4683a94a322
hesher <ben.katz@gmail.com>
2014-08-12 14:34:15 +0300

    trying different things with the url + real image

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,23 +5,41 @@
 
   /* @ngInject */
   function MainController($scope, $http) {
+    var image;
+    var ready = false;
+
+    $http.get('https://dl.dropboxusercontent.com/u/80497/photoHosting/design.png')
+    .success(function (res) {
+      image = res;
+      ready = true;
+      console.log('succeeded, img.length:' + image.length);
+    }).error(function (res, status) {
+      console.log('failed, status:' + status);
+    });
 
     $scope.test = 'hello';
     $scope.doPut = function () {
       console.log('putting...');
       $http({
         method: 'put',
-        url: 'http://static.pizza.wixpress.com/_api/statics/add_file_named2',
+        //url: 'http://static.pizza.wixpress.com/_api/add_file_named2',
+        url: 'http://static.pizza.wixpress.com/_api/add_file_named2',
         headers: {
           'X-Prospero-Filename': '98080808098.jpg',
           'X-Prospero-Mediasource': 'media'
         },
-        data: 'alert(\'hello\');'
+        data: image
       }).success(function (data, status) {
+        debugger;
         alert('** SUCCESS ** Status:' + status);
       }).error(function (data, status) {
+        debugger;
         alert('** FAILED ** Status:' + status);
       });
+    };
+
+    $scope.isReady = function () {
+      return ready;
     };
 
   }
